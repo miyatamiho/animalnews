54f306523dca2144325e4afbfe2c455ece07f23f
miyatamiho <t.td.k45@gmail.com>
2023-04-12 12:26:52 +0000

    確認済

--- resources/views/topic/index.blade.php
+++ resources/views/topic/index.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="row">
             <div class="col-md-4">
-                <a href="#" role="button" class="btn btn-primary">新規作成</a>
+                <a href="{{ route('topic.create') }}"  role="button" class="btn btn-primary">新規作成</a>
             </div>
             <div class="col-md-8">
                 <form action="{{ route('topic.index') }}" method="get">
